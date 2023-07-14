366ed92ae33e8a288b8fbfa552bfa9d99c99b0f5
Federico Sabiche <fsabiche>
2023-07-14 18:13:57 -0300

    modificaciones varias funcional de BD y servidor

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ import express from 'express';
 import chalk from 'chalk';
 import morgan from 'morgan';
 import path from 'path';
-import authRouter from './src/routers/authRouter.js';
+import authRouter from './routers/authRouter.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
-import passportConfig from './src/config/passport.js';
-import flash from 'connect-flash'
-import productsRouter from './src/routers/productsRouter.js';
-import adminProductsRouter from './src/routers/adminProductsRouter.js';
+import passportConfig from './config/passport.js';
+import flash from 'connect-flash';
+import productsRouter from './routers/productsRouter.js';
+import adminProductsRouter from './routers/adminProductsRouter.js';
 
 
 
 dotenv.config({ path: `./.env` });
 
-const PORT = process.env.PORT || 3050;
+const PORT = process.env.PORT || 3030;
 
 const __dirname = path.resolve();
 const app = express()
@@ -26,23 +26,40 @@ app.use(morgan('tiny'));
 // middlerware para q lea archivos desde el body
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-//cookie de passport
+/*
+//cookie de passport   // lo saque por me tiraba error!!!!!!!
 app.use(cookieParser());
 app.use(session({ secret: 'globomantics' }));
+*/
 
+//conectamos app.session formulada por IA!!!!!!!!!
+app.use(session({
+    secret: 'my-secret-key',
+    resave: true,
+    saveUninitialized: true
+  }));
+  
 // traigo la funcion de passport
 passportConfig(app);
 
 // para los mensajes flash connect
 app.use((req, res, next) => {
-    res.locals.success_msg = req.flash(('success_msg'));
-    res.locals.error_msg = req.flash(('error_msg'));
-    res.locals.error = req.flash(('error'));
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
     res.locals.currentUser = req.user;
     next();
 });
 app.use(flash());
+
+//probando error de express session sacada de IA!!!!!!!!!!
+app.use(session({
+    secret: 'my-secret-key',
+    resave: false,
+    saveUninitialized: false
+  }));
+  
+
 
 //carpeta de uso estatico
 app.use(express.static('public'));
@@ -57,7 +74,8 @@ app.set('views', __dirname + '/views')
 // rutas
 app.use('/auth', authRouter);
 app.use('/products', productsRouter);
-app.use('/adminProducts', adminProductsRouter);
+app.use('./adminProducts', adminProductsRouter);
+
 
 //rutas views index.ejs
 app.get('/',(req,res)=>{
@@ -98,10 +116,23 @@ app.use((req,res,next) => {
 app.listen(PORT,()=>{
     console.log('el servidor esta conectado en el puerto',PORT)
 })
-
+/*  // cambie el mongoose porque no funcionaba con ese codigo!!!!
 mongoose
     .connect(process.env.DB)
     .then(mensaje => {
         console.log('DB CONECTADA')
     })
     .catch(error => console.log(error.stack));
+
+*/
+// conexion a Mongoose nueva (fijate que en mi laptop tengo que usar la IP para que me funcione el mongoDB)
+mongoose.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+console.log('Conexión a la base de datos exitosa');
+
+
+    // Resto del código de tu aplicación
+  })
+  .catch((error) => {
+    console.error('Error al conectar a la base de datos:', error);
+  });
